09c7dd6801d01ebffd7fe224323bac2c3a43a0bc
Karolina Kazmierska <karo.a.kazmierska@gmail.com>
2020-04-18 15:29:39 +0200

    Updates

---
+++ precache-manifest.ba17cf6efc32edcdc6ff0100c7c92006.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b39eee320e5a1a93331ed05f6f45bf",
+    "url": "/invoice-management-program/index.html"
+  },
+  {
+    "revision": "e548228e3f5811c20322",
+    "url": "/invoice-management-program/static/js/2.72ec2344.chunk.js"
+  },
+  {
+    "revision": "86414f6aa96d78f04460ff2dda206a29",
+    "url": "/invoice-management-program/static/js/2.72ec2344.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d08e18de0db73b7fd9cd",
+    "url": "/invoice-management-program/static/js/main.95e6aff6.chunk.js"
+  },
+  {
+    "revision": "7dee27bffe151c3cebaa",
+    "url": "/invoice-management-program/static/js/runtime-main.5284b35d.js"
+  }
+]);
